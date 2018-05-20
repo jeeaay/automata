077f80398c2617f1756e0b9e5f71f83b81906ec8
jeay <wrjie@msn.cn>
2018-05-20 16:06:56 +0800

    使用rsa密钥登录

--- ssh.py
+++ ssh.py
@@ -6,29 +6,30 @@
  * @Author: Jeay 
  * @Date: 2018-05-20 10:32:09 
  * @Last Modified by: Jeay
- * @Last Modified time: 2018-05-20 14:01:04
+ * @Last Modified time: 2018-05-20 16:03:35
  */
 '''
 
-import paramiko
+import paramiko, os
 
 import cert
 
-private_key = cert.cert().get_privkey()
+cert.cert()
+private_key = paramiko.RSAKey.from_private_key_file(os.path.expanduser('~') + '/.ssh/id_rsa')
+
 # 创建SSH对象
 ssh = paramiko.SSHClient()
 # 允许连接不在know_hosts文件中的主机
 ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())#第一次登录的认证信息
 # 连接服务器
+
 try:
   ssh.connect(hostname='45.32.135.211', port=22, username='root', pkey=private_key)
-  print(1)
 except Exception as e:
   ssh.connect(hostname='45.32.135.211', port=22, username='root', password='Qj-1.7d?uHKQWahW')
-  public_key = cert.cert().get_loginkey()
+  public_key = cert.cert().get_pubkey()
   # 写入公钥
   ssh.exec_command('echo "' + public_key + '">> ~/.ssh/authorized_keys')
-  print(2)
 
 #ssh.connect(hostname='45.32.135.211', port=22, username='root', password='Qj-1.7d?uHKQWahW')
 # 执行命令
